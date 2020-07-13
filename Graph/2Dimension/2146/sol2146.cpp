7f3bac45ad6ed89d0186c18ccca033845aa919a6
david4u <jeehyun990@yonsei.ac.kr>
2020-07-13 23:26:10 +0900

    .

--- Graph/2Dimension/2146/sol2146.cpp
+++ Graph/2Dimension/2146/sol2146.cpp
@@ -11,6 +11,46 @@ int N;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
+int min(int x, int y) {
+	return x > y ? y : x;
+}
+
+int BFS(int x) {
+    queue<pair<int, int>> q;
+    for (int i = 1 ; i<= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            if(map[i][j] == x) {
+                visited[i][j] = true;
+                q.push({j, i});
+            }
+        }
+    }
+    int result = 0;
+    while (!q.empty()) {
+        int size = q.size();
+        for (int i = 0; i < size; i++) {
+            int cx = q.front().first;
+            int cy = q.front().second;
+            q.pop();
+            
+            for (int j = 0 ; j <4; j++) {
+                int ny = cy + dy[j];
+                int nx = cx + dx[j];
+                if ( 1 <= nx && nx <= N && 1 <= ny && ny <= N) {
+                    if (map[ny][nx] != 0 && map[ny][nx] != x) {
+                        return result;
+                    }
+                    else if (map[ny][nx] == 0 && !visited[ny][nx]) {
+                        visited[ny][nx] = true;
+                        q.push({nx, ny});
+                    }
+                }
+            }
+        }
+		result++;
+    }
+}
+
 int main() {
 	cin >> N;
 	for (int i = 1; i <= N; i++) {
@@ -22,6 +62,7 @@ int main() {
 	int count = 0;
 	bool more = false;
 	queue<pair<int, int>> q;
+	queue<pair<int, int>> edge;
 	for (int i = 1;i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
 			if (map[i][j] == 1) {
@@ -46,6 +87,9 @@ int main() {
 				if ( 1<= nx && nx <= N && 1 <= ny && ny <= N && !visited[ny][nx] && map[ny][nx] == 1) {
 					q.push({nx, ny});
 				}
+				if ( 1<= nx && nx <= N && 1 <= ny && ny <= N && !visited[ny][nx] && map[ny][nx] == 0) {
+					edge.push({cx, cy});
+				}
 			}
 		}
 		for (int i = 1;i <= N; i++) {
@@ -58,14 +102,10 @@ int main() {
 			}
 		}
 	}
-	// 여기까지 map setting. 섬들을 1번 2번 3번...으로 나눴음.
-	// 이제 각 섬들에 대해서 영토 하나씩 넓혀가면서 다른 섬에 얼마나 넓혀야 닿는지 최소값 구하고 그중 최고값 출력.
-	// cout << '\n';
-	// for (int i = 1; i <= N; i++) {
-	// 	for (int j = 1; j <= N; j++) {
-	// 		cout << map[i][j];
-	// 	}
-	// 	cout << '\n';
-	// }
-
+	int answer = 10000;
+    for (int i = 1; i <= count; i++) {
+        memset(visited, false, sizeof(visited));
+        answer = min(answer, BFS(i));
+    }
+	cout << answer << '\n';
 }
